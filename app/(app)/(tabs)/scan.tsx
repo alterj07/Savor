f4b48337810fc25f03d81be8ec2aa1ccdbf40fa1
Jayden Chun <jayden.h.chun@gmail.com>
2026-06-17 20:00:56 +0900

    feat: added basic scan page in (app)/(tabs) folders

---
+++ app/(app)/(tabs)/scan.tsx
@@ -0,0 +1,10 @@
+// app/(app)/(tabs)/scan.tsx
+import { View, Text } from 'react-native';
+
+export default function ScanScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Scan Screen — Week 3</Text>
+    </View>
+  );
+}
